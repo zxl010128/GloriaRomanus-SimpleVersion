5c93dbf92d8347db627b15da16823b0e1eee7f44
Pai Qu <pai.qu@outlook.com>
2020-10-28 21:48:25 +1100

    create some classes to begin

---
+++ src/unsw/gloriaromanus/backend/Faction.java
@@ -0,0 +1,27 @@
+package unsw.gloriaromanus.backend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Faction {
+    private String name;
+    private int balance;
+    // int soldiersNum; Do we need this one?
+    private List<Province> provinces;
+
+    public Faction(String name, Province startingProvince) {
+        this.name = name;
+        this.balance = 100;
+        this.provinces = new ArrayList<Province>();
+        provinces.add(startingProvince);
+    }
+
+    public void addProvince(Province p) {
+        provinces.add(p);
+    }
+
+    public void removeProvince(Province p) {
+        provinces.remove(p);
+    }
+
+}
