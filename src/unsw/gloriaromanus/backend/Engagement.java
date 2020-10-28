e2b2dec4193b4caac41c67cea34a0455ad9f11a2
Pai Qu <pai.qu@outlook.com>
2020-10-28 22:39:30 +1100

    change class design

--- src/unsw/gloriaromanus/backend/Engagement.java
+++ src/unsw/gloriaromanus/backend/Engagement.java
@@ -1,6 +1,27 @@
 package unsw.gloriaromanus.backend;
 
+import java.util.Random;
+
 public class Engagement {
-    private Soldier attacker;
-    private Soldier defender;
+    private Troop attacker;
+    private Troop defender;
+    private String type;
+
+    public Engagement(Troop attacker, Troop defender) {
+        this.attacker = attacker;
+        this.defender = defender;
+
+        String attackerType = attacker.getType();
+        String defenderType = defender.getType();
+
+        if (attackerType.equals("melee") && defenderType.equals("melee")) {
+            this.type = "melee";
+        } else if (attackerType.equals("missile") && defenderType.equals("missile")) {
+            this.type = "missile";
+        } else {
+            Random r = new Random();
+            int choice = r.nextInt(2);
+            
+        }
+    }
 }
