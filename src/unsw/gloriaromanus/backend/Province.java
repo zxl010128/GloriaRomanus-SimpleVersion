8f5ee4a688e09a44a3ce7762c1453bd9933c2432
Pai Qu <pai.qu@outlook.com>
2020-10-30 22:22:58 +1100

    save before merging

--- src/unsw/gloriaromanus/backend/Province.java
+++ src/unsw/gloriaromanus/backend/Province.java
@@ -2,6 +2,7 @@ package unsw.gloriaromanus.backend;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Province {
     private String name;
@@ -9,13 +10,16 @@ public class Province {
     private double taxRate;
     private Faction faction;
     private List<Unit> units;
+    private List<Double> taxRates;
 
-    public Province(String name) {
+    public Province(String name, Faction faction) {
         this.name = name;
-        this.wealth = 0;
+        this.wealth = 100;
 
         // assume the default tax is normal tax (0.15)
         this.taxRate = 0.15;
+
+        this.faction = faction;
         this.units = new ArrayList<Unit>();
     }
 
@@ -24,7 +28,9 @@ public class Province {
      * recruit soldier for a province
      * @param type soldier type
      */
-    public void recruit(String type) {
+    public void recruit(String type, int currTurn) {
+        // Read the UnitsInfo.JSON
+
         // parameter may be changed later
         switch (type) {
             // implement later after fininshing soldier class
@@ -94,6 +100,55 @@ public class Province {
 
     public String getName() {
         return name;
+    }
+
+    public void setTaxRate(double taxRate) {
+        // initialize  a double list to store available taxRates
+        this.taxRates = new ArrayList<Double>();
+        taxRates.add(0.1);
+        taxRates.add(0.15);
+        taxRates.add(0.2);
+        taxRates.add(0.25);
+
+        if (taxRates.contains(taxRate)) {
+            this.taxRate = taxRate;
+        } else {
+            System.out.println("invalid tax rate!");
+        }
+    }
+
+    public int getTaxRevenue() {
+        return (int) Math.rint(wealth * taxRate);
+    }
+
+    public int getWealth() {
+        return wealth;
+    }
+
+    public void update() {
+        // this function gets called each turn
+        // update townealth
+        switch ((int) (taxRate * 100)) {
+            case 10: 
+                // low tax
+                wealth += 10;
+                break;
+            case 15: 
+                // normal tax
+                break;
+            case 20: 
+                // high tax
+                wealth -= 10;
+                break;
+            case 25:
+                // very high tax
+                wealth -= 30;
+                break;
+        }
+
+        if (wealth < 0) {
+            wealth = 0;
+        }
     }
 
 }
